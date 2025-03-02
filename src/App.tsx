c3acf7616363f8ca1d6fec7cdec808e70b9967ba
yeeZinu <doglife222@gmail.com>
2025-03-02 14:51:48 +0900

    project setting

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+function App() {
+
+  return (
+    <Main>
+      hi 모두들 안녕 내가 누군지 아니
+    </Main>
+  )
+}
+
+export default App
+
+const Main = styled.div`
+  display: flex;
+  flex-directon: center;
+`;
