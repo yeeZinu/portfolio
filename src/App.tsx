2b0e12cb41f25f21f3ff56c1588aa049edbebe9f
yeeZinu <cube1008@naver.com>
2025-03-09 12:01:19 +0900

    click 시 카메라 뷰 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import * as THREE from "three";
 import React, { useRef, useState } from "react";
-import { Canvas, useFrame, ThreeElements, useLoader } from "@react-three/fiber";
+import {
+  Canvas,
+  useFrame,
+  ThreeElements,
+  useLoader,
+  useThree,
+  ThreeEvent,
+} from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 function Box(props: ThreeElements["mesh"]) {
   const meshRef = useRef<THREE.Mesh>(null!);
@@ -24,10 +31,32 @@ function Box(props: ThreeElements["mesh"]) {
   );
 }
 
+function Strawberry() {
+  const gltf = useLoader(GLTFLoader, "models/strawberrySub.glb");
+  const { camera } = useThree();
+
+  const handleClick = (event: ThreeEvent<MouseEvent>) => {
+    if (!event.object) return;
+    const object = event.object;
+    camera.position.set(
+      object.position.x + 2,
+      object.position.y + 2,
+      object.position.z + 5
+    );
+  };
+
+  return (
+    <mesh onClick={handleClick}>
+      <primitive
+        object={gltf.scene}
+        position={[0, 1, 0]}
+        children-0-castShadow
+      />
+    </mesh>
+  );
+}
+
 export default function App() {
-
-  const gltf = useLoader(GLTFLoader, 'models/strawberrySub.glb');
-
   return (
     <Canvas>
       <ambientLight intensity={Math.PI / 2} />
@@ -39,14 +68,10 @@ export default function App() {
         intensity={Math.PI}
       />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <primitive
-        object={gltf.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
+      <Strawberry />
       {/* <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} /> */}
-      <OrbitControls/>
+      <OrbitControls />
     </Canvas>
   );
 }
