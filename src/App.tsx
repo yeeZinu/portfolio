9140e5189b46b135124e00cca01d0d599b7e60fb
yeeZinu <cube1008@naver.com>
2025-03-09 17:02:19 +0900

    특정 오브젝트 가져오기 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,17 @@ function Strawberry() {
   const gltf = useLoader(GLTFLoader, "models/strawberrySub.glb");
   const { camera } = useThree();
 
+  // 3d 모델 크기 확인
+  // const boxHelper = new THREE.BoxHelper(gltf.scene, 0x00ff00);
+  // gltf.scene.add(boxHelper);
+
+  // 배경 제거거
+  const background = gltf.scene.getObjectByName("평면002");
+  if (background) {
+    background.visible = false;
+  }
+
+  // 클릭 시 특정 위치로 이동동
   const handleClick = (event: ThreeEvent<MouseEvent>) => {
     if (!event.object) return;
     const object = event.object;
@@ -46,13 +57,12 @@ function Strawberry() {
   };
 
   return (
-    <mesh onClick={handleClick}>
-      <primitive
-        object={gltf.scene}
-        position={[0, 1, 0]}
-        children-0-castShadow
-      />
-    </mesh>
+    <primitive
+      object={gltf.scene}
+      children-0-castShadow
+      onClick={handleClick}
+      scale={0.5}
+    />
   );
 }
 
@@ -72,6 +82,7 @@ export default function App() {
       {/* <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} /> */}
       <OrbitControls />
+      <gridHelper args={[10, 10]} />
     </Canvas>
   );
 }
